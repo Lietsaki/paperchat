cd7ee3c72cb5232299419a7a4f990526a903151d
Ricardo <ricardosandez67@gmail.com>
2022-07-07 22:21:10 +0200

    Created initial layout

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
+const path = require('path')
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+    prependData: `@import "main.scss";`
+  }
 }
 
 module.exports = nextConfig
