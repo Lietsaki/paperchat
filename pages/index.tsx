04d3f6120ec8a272082c5f72327f30acaf4118a1
Ricardo <ricardosandez67@gmail.com>
2022-07-09 23:19:57 +0200

    Added styles for home screens' background

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,13 @@
 import styles from '../styles/layout/layout.module.scss';
 
-const { main, main_section, screen } = styles;
+const { main, main_section, screen, top, bottom } = styles;
 
 const Home = () => {
   return (
     <div className={main}>
       <div className={main_section}>
-        <div className={screen}></div>
-        <div className={screen}></div>
+        <div className={`${screen} ${top}`}></div>
+        <div className={`${screen} ${bottom}`}></div>
       </div>
     </div>
   );
