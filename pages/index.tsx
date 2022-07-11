abbdb5a3815cbaa4e31427140db000a5fb8570a2
Ricardo <ricardosandez67@gmail.com>
2022-07-11 22:02:14 +0200

    Added initial grid styles for the bottom home screen

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,25 @@
-import styles from 'styles/home/home.module.scss';
+import styles from 'styles/home/home.module.scss'
 
-const { top, bottom } = styles;
+const { top, bottom } = styles
 
 const Home = () => {
   return (
     <div className="main">
       <div className="main_section">
         <div className={`screen ${top}`}></div>
-        <div className={`screen ${bottom}`}></div>
+        <div className={`screen ${bottom}`}>
+          <div>Find Rooms</div>
+          <div>Create a Room</div>
+          <div>I have a code</div>
+
+          <div>
+            <div>Username</div>
+            <div>Cat12345</div>
+          </div>
+        </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
