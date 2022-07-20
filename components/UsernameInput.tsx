03bb1a7bcf3611d07ca1dd4e0340df1bef4c397f
Ricardo <ricardosandez67@gmail.com>
2022-07-20 08:59:21 +0200

    Autofocus username input when clicking to edit it

--- components/UsernameInput.tsx
+++ components/UsernameInput.tsx
@@ -1,3 +1,4 @@
+import { useRef, useEffect } from 'react'
 import styles from 'styles/components/username-input.module.scss'
 
 const { input_container, title, username_input, make_complete_rectangle } =
@@ -8,13 +9,19 @@ type UsernameInputProps = {
 }
 
 const UsernameInput = ({ editing }: UsernameInputProps) => {
-  const shouldBeCompleteRectangle = () =>
-    editing ? make_complete_rectangle : ''
+  const inputRef = useRef<HTMLInputElement>(null)
+  const shouldBeCompleteRectangle = () => {
+    return editing ? make_complete_rectangle : ''
+  }
+
+  useEffect(() => {
+    if (editing && inputRef.current) inputRef.current.focus()
+  }, [editing])
 
   return (
     <div className={`${input_container} ${shouldBeCompleteRectangle()}`}>
       <div className={title}>Username</div>
-      <input type="text" className={username_input}></input>
+      <input ref={inputRef} type="text" className={username_input}></input>
     </div>
   )
 }
